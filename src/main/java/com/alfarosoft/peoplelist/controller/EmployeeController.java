0b84b49bd4dba18a92727e14dd60e7eea5b8fb01
Mauro Alfaro <mauroalfaroo@gmail.com>
2020-06-22 22:08:31 -0300

    Added Swagger UI

--- src/main/java/com/alfarosoft/peoplelist/controller/EmployeeController.java
+++ src/main/java/com/alfarosoft/peoplelist/controller/EmployeeController.java
@@ -4,6 +4,11 @@ import com.alfarosoft.peoplelist.exception.AddressValidationException;
 import com.alfarosoft.peoplelist.exception.PeopleListException;
 import com.alfarosoft.peoplelist.model.Employee;
 import com.alfarosoft.peoplelist.validation.AddressValidation;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,27 +31,72 @@ public class EmployeeController {
         this.addressValidation = addressValidation;
     }
 
+    @Operation(summary = "Adds a employee to the database")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "202", description = "Employee successfully created",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Employee.class)) }),
+            @ApiResponse(responseCode = "400", description = "Invalid body applied",
+                    content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal server error",
+                    content = @Content)})
     @PostMapping(value = "/add", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
     public ResponseEntity<Employee> addEmployee (@RequestBody Employee employee){
         addressValidation.validateAddress(employee.getAddress());
         return ResponseEntity.status(HttpStatus.ACCEPTED).body(employeeService.addEmployee(employee));
     }
 
+    @Operation(summary = "Searches for all the Employees on the database")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Employees found",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Employee.class)) }),
+            @ApiResponse(responseCode = "404", description = "Employees not found",
+                    content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal server error",
+                    content = @Content)})
     @GetMapping(produces = APPLICATION_JSON_VALUE)
     public ResponseEntity<List<Employee>> searchEmployees(){
         return ResponseEntity.status(HttpStatus.OK).body(employeeService.getEmployees());
     }
 
+    @Operation(summary = "Searches a Employee by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Employee found",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Employee.class)) }),
+            @ApiResponse(responseCode = "404", description = "Employee not found",
+                    content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal server error",
+                    content = @Content)})
     @GetMapping(value = "/{id}", produces = "application/json")
     public ResponseEntity<Employee> lookupEmployee (@PathVariable String id){
         return ResponseEntity.status(HttpStatus.OK).body(employeeService.getEmployee(id));
     }
 
+    @Operation(summary = "Updates Employee data after finding it by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Employee updated",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Employee.class)) }),
+            @ApiResponse(responseCode = "404", description = "Employee not found",
+                    content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal server error",
+                    content = @Content)})
     @PutMapping(value = "/update/{id}", produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE)
     public ResponseEntity<Employee> updateEmployee (@PathVariable String id, @RequestBody Employee employee){
         return ResponseEntity.status(HttpStatus.OK).body(employeeService.updateEmployee(id, employee));
     }
 
+    @Operation(summary = "Deletes a Employee after finding it by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Employee deleted",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Employee.class)) }),
+            @ApiResponse(responseCode = "404", description = "Employee not found",
+                    content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal server error",
+                    content = @Content)})
     @DeleteMapping(value = "/{id}", produces = APPLICATION_JSON_VALUE)
     public ResponseEntity<String> deleteEmployee(@PathVariable String id){
         employeeService.removeEmployee(id);
--- src/main/java/com/alfarosoft/peoplelist/controller/StoreController.java
+++ src/main/java/com/alfarosoft/peoplelist/controller/StoreController.java
@@ -4,6 +4,11 @@ import com.alfarosoft.peoplelist.exception.AddressValidationException;
 import com.alfarosoft.peoplelist.exception.PeopleListException;
 import com.alfarosoft.peoplelist.model.Store;
 import com.alfarosoft.peoplelist.validation.AddressValidation;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -25,27 +30,72 @@ public class StoreController {
         this.addressValidation = addressValidation;
     }
 
+    @Operation(summary = "Adds a Store to the database")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "202", description = "Store successfully created",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Store.class)) }),
+            @ApiResponse(responseCode = "400", description = "Invalid body applied",
+                    content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal server error",
+                    content = @Content)})
     @PostMapping(value = "/add", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
     public ResponseEntity<Store> addStore(@RequestBody Store store){
         addressValidation.validateAddress(store.getAddress());
         return ResponseEntity.status(HttpStatus.ACCEPTED).body(storeService.addStore(store));
     }
 
-    @GetMapping(produces = APPLICATION_JSON_VALUE)
-    public ResponseEntity<List<Store>> searchStores(){
-        return ResponseEntity.status(HttpStatus.OK).body(storeService.getStores());
-    }
-
+    @Operation(summary = "Searches a Store by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Store found",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Store.class)) }),
+            @ApiResponse(responseCode = "404", description = "Store not found",
+                    content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal server error",
+                    content = @Content)})
     @GetMapping(value = "/{id}", produces = "application/json")
     public ResponseEntity<Store> lookupStore (@PathVariable String id){
         return ResponseEntity.status(HttpStatus.OK).body(storeService.getStore(id));
     }
 
+    @Operation(summary = "Searches for all the Stores on the database")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Stores found",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Store.class)) }),
+            @ApiResponse(responseCode = "404", description = "Stores not found",
+                    content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal server error",
+                    content = @Content)})
+    @GetMapping(produces = APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<Store>> searchStores(){
+        return ResponseEntity.status(HttpStatus.OK).body(storeService.getStores());
+    }
+
+    @Operation(summary = "Updates Store data after finding it by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Store updated",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Store.class)) }),
+            @ApiResponse(responseCode = "404", description = "Store not found",
+                    content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal server error",
+                    content = @Content)})
     @PutMapping(value = "/update/{id}", produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE)
     public ResponseEntity<Store> updateStore (@PathVariable String id, @RequestBody Store store){
         return ResponseEntity.status(HttpStatus.OK).body(storeService.updateStore(id, store));
     }
 
+    @Operation(summary = "Deletes a Store after finding it by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Store deleted",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Store.class)) }),
+            @ApiResponse(responseCode = "404", description = "Store not found",
+                    content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal server error",
+                    content = @Content)})
     @DeleteMapping(value = "/{id}", produces = APPLICATION_JSON_VALUE)
     public ResponseEntity<String> deleteStore(@PathVariable String id){
         storeService.removeStore(id);
